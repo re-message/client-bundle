c8f5d97a3e949d95394a3a6eebad4711e60f18a4
Oleg Kozlov <h1karo@outlook.com>
2020-06-24 23:02:17 +0300

    Update copyright

--- src/EventListener/ServiceAuthenticatorListener.php
+++ src/EventListener/ServiceAuthenticatorListener.php
@@ -6,8 +6,9 @@
  * @link      https://github.com/relmsg/client-bundle
  * @link      https://dev.relmsg.ru/packages/client-bundle
  * @copyright Copyright (c) 2018-2020 Relations Messenger
- * @author    h1karo <h1karo@outlook.com>
+ * @author    Oleg Kozlov <h1karo@outlook.com>
  * @license   https://legal.relmsg.ru/licenses/client-bundle
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
