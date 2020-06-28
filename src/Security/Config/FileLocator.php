d53e675dcc9fbe05441b271b5be34ff9ec4313f8
Oleg Kozlov <h1karo@outlook.com>
2020-06-28 03:01:24 +0300

    Replace personal email with @relmsg.ru

--- src/Security/Config/FileLocator.php
+++ src/Security/Config/FileLocator.php
@@ -6,7 +6,7 @@
  * @link      https://github.com/relmsg/client-bundle
  * @link      https://dev.relmsg.ru/packages/client-bundle
  * @copyright Copyright (c) 2018-2020 Relations Messenger
- * @author    Oleg Kozlov <h1karo@outlook.com>
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
  * @license   https://legal.relmsg.ru/licenses/client-bundle
  *
  * For the full copyright and license information, please view the LICENSE
@@ -22,7 +22,7 @@ use Symfony\Component\Config\FileLocator as BaseFileLocator;
 /**
  * Class FileLocator
  *
- * @author Oleg Kozlov <h1karo@outlook.com>
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
  */
 class FileLocator extends BaseFileLocator
 {
